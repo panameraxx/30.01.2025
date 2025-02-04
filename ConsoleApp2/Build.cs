e10365928dfc0953b5284abb16827b2ebf65ee0d
panameraxx <90213411+panameraxx@users.noreply.github.com>
2025-02-04 21:04:36 +0200

    Add files via upload

---
+++ ConsoleApp2/Build.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Build
+    {
+        public string title;
+        public double size;
+        public int residents;
+        public int floors;
+
+        public Build() { }
+
+        public Build(string title)
+        {
+            this.title = title;
+        }
+        public Build(string title, double size) : this(title)
+        {
+            this.size = size;
+        }
+        public Build(string title, double size, int residents) : this(title, size)
+        {
+            this.residents = residents;
+        }
+        public Build(string title, double size, int residents, int floors) : this(title, size, residents)
+        {
+            this.floors = floors;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"В доме: {title}\t Площадью: {size}\t Квадратных метров живет: {residents} чел," +
+                $"на человека - {size / residents:f2} кв. метров");
+        }
+        public override string ToString()
+        {
+            return $"В доме {title} площадью {size} кв. метров живёт {residents} чел, " +
+                $"на человека - {size / residents:f2} кв. метров";
+        }
+    }
+
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Build house1 = new Build();
+            house1.title = "Квартира";
+            house1.size = 80;
+            house1.residents = 2;
+            house1.ShowInfo();
+            Console.WriteLine();
+
+            // Второй дом
+            Build house2 = new Build("Отель", 6000, 1500);
+            Console.WriteLine($"{house2.ToString()}");
+            Console.WriteLine();
+
+            Console.Write("Название строения: ");
+            string title = Console.ReadLine();
+            Console.Write("Площадь строения: ");
+            double size = double.Parse(Console.ReadLine());
+            Console.Write("Количество жильцов: ");
+            int residents = int.Parse(Console.ReadLine());
+            Console.Write("Количество этажей: ");
+            int floors = int.Parse(Console.ReadLine());
+
+            Build house3 = new Build(title, size, residents, floors);
+            Console.WriteLine($"{house3.ToString()}");
+            Console.ReadKey();
+
+        }
+    }
+}
