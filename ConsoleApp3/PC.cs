57c21c6f8db728cacb2be4b23063423130ac2381
panameraxx <90213411+panameraxx@users.noreply.github.com>
2025-02-04 21:21:38 +0200

    Add files via upload

---
+++ ConsoleApp3/PC.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+        internal class PC
+        {
+            public string mod;
+            public int freq;
+            public int ram;
+            public int mem;
+
+            public PC() { }
+
+            public PC(string mod)
+            {
+                this.mod = mod;
+            }
+
+            public PC(string mod, int freq) : this(mod)
+            {
+                this.freq = freq;
+            }
+
+            public PC(string mod, int freq, int ram) : this(mod, freq)
+            {
+                this.ram = ram;
+            }
+
+            public PC(string mod, int freq, int ram, int mem) : this(mod, freq, ram)
+            {
+                this.mem = mem;
+            }
+
+            public void Info()
+            {
+                Console.WriteLine($"Модель: {mod}\t Тактовая частота: {freq}\t ОЗУ: {ram} ГБ\t Память: {mem} ГБ");
+            }
+
+            public override string ToString()
+            {
+                return $"Модель: {mod}\t Тактовая частота: {freq}\t ОЗУ: {ram} ГБ\t Память: {mem} ГБ";
+            }
+        }  
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PC pc1 = new PC();
+            pc1.mod = "AMD";
+            pc1.freq = 2;
+            pc1.ram = 8;
+            pc1.mem = 256;
+
+            Console.WriteLine();
+            //
+            Console.WriteLine("Модель: ");
+            string mod = Console.ReadLine();
+            Console.WriteLine("Тактовая частота: ");
+            int frq = int.Parse(Console.ReadLine());
+            Console.WriteLine("Оперативная память: ");
+            int rm = int.Parse(Console.ReadLine());
+            Console.WriteLine("Объем памяти: ");
+            int mem = int.Parse(Console.ReadLine());
+
+            pc1.Info();
+            PC pc2 = new PC(mod, frq, rm, mem);
+            Console.WriteLine($"{pc2.ToString()}");
+            //
+            PC pc3 = new PC("Intel", 3, 32, 512);
+            Console.WriteLine($"{pc3.ToString()}");
+            Console.ReadKey();
+
+        }
+    }
+}
